bc93ba8738e659e46b266c12119cc9bb78634e9e
Ryan <Jaconah@users.noreply.github.com>
2016-02-22 13:40:54 -0600

    Update PushBotTelemetry.java
    shorten and added right bumper in to the code for the telementry to send to driver station

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotTelemetry.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/PushBotTelemetry.java
@@ -4,35 +4,6 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 //
 // PushBotTelemetry
 //
-/**
- * Provide telemetry provided by the PushBotHardware class.
- *
- * Insert this class between a custom op-mode and the PushBotHardware class to
- * display telemetry available from the hardware class.
- *
- * @author SSI Robotics
- * @version 2015-08-02-13-57
- *
- * Telemetry Keys
- *     00 - Important message; sometimes used for error messages.
- *     01 - The power being sent to the left drive's motor controller and the
- *          encoder count returned from the motor controller.
- *     02 - The power being sent to the right drive's motor controller and the
- *          encoder count returned from the motor controller.
- *     03 - The power being sent to the left arm's motor controller.
- *     04 - The position being sent to the left and right hand's servo
- *          controller.
- *     05 - The negative value of gamepad 1's left stick's y (vertical; up/down)
- *          value.
- *     06 - The negative value of gamepad 1's right stick's y (vertical;
- *          up/down) value.
- *     07 - The negative value of gamepad 2's left stick's y (vertical; up/down)
- *          value.
- *     08 - The value of gamepad 2's X button (true/false).
- *     09 - The value of gamepad 2's Y button (true/false).
- *     10 - The value of gamepad 1's left trigger value.
- *     11 - The value of gamepad 1's right trigger value.
- */
 public class PushBotTelemetry extends PushBotHardware
 
 {
@@ -49,14 +20,6 @@ public class PushBotTelemetry extends PushBotHardware
 
     {
         //
-        // Initialize base classes.
-        //
-        // All via self-construction.
-
-        //
-        // Initialize class members.
-        //
-        // All via self-construction.
 
     } // PushBotTelemetry
 
@@ -91,28 +54,7 @@ public class PushBotTelemetry extends PushBotHardware
                 + ", "
                 + a_right_encoder_count ()
             );
-        telemetry.addData
-            ( "03"
-            , "Left Arm: " + a_left_arm_power ()
-            );
-        telemetry.addData
-            ( "04"
-            , "Hand Position: " + a_hand_position ()
-            );
-        telemetry.addData
-                ( "01"
-                        , "Left Drive2: "
-                                + a_left_drive_power2 ()
-                                + ", "
-                                + a_left_encoder_count ()
-                );
-        telemetry.addData
-                ( "02"
-                        , "Right Drive2: "
-                                + a_right_drive_power2 ()
-                                + ", "
-                                + a_right_encoder_count ()
-                );
+       
     } // update_telemetry
 
     //--------------------------------------------------------------------------
@@ -130,11 +72,7 @@ public class PushBotTelemetry extends PushBotHardware
         //
         telemetry.addData ("05", "GP1 Left: " + -gamepad1.left_stick_y);
         telemetry.addData ("06", "GP1 Right: " + -gamepad1.right_stick_y);
-        telemetry.addData ("07", "GP2 Left: " + -gamepad2.left_stick_y);
-        telemetry.addData ("08", "GP2 X: " + gamepad2.x);
-        telemetry.addData ("09", "GP2 Y: " + gamepad2.y);
-        telemetry.addData ("10", "GP1 LT: " + gamepad1.left_trigger);
-        telemetry.addData ("11", "GP1 RT: " + gamepad1.right_trigger);
+        telemetry.addData ("07", "GP1 RB: " + gamepad1.right_bumper);
 
     } // update_gamepad_telemetry
 
